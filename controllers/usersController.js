78192d0795c2ee506243c902b7df2bd18960bdb4
jomeluckye <emajumeck80@gmail.com>
2022-08-06 10:20:39 -0700

    created userscontroller.js, user.js files

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,2 +1,11 @@
 const User = require('../models/User');
+const { validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
+//@route      POST api/auth/login
+//@Desc       Auth user(Student, Tutor, Admin) and get token
+//@Access     Public
+exports.loginUser = async (req, res) =>{
+    //Check
+}
